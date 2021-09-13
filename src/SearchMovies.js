df2a1ba9a922a1ab99a42edddc093c832691a21b
Jennifer Toops <jennifertoops@gmail.com>
2021-09-13 17:07:34 -0400

    added the useState hook for the query

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
+
+export default function SearchMovies() {
+//set query useState hook
+const [query, setQuery] = useState('');
 
 const searchMovies = async (e) => {
     e.preventDefault();
     console.log("submitting");
     
-    const query = "little women"
     try {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=d8955e427ed83abc4f49a8b15207ec3e&language=en-US&page=1&query=${query}&include_adult=false`;
     const res = await fetch(url);
@@ -15,11 +18,10 @@ const searchMovies = async (e) => {
     }
 }
 
-export default function SearchMovies() {
     return (
         <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query" >Movie Name</label>
-            <input className="input" type="text" name="query" placeholder="enter a movie" />
+            <input className="input" type="text" name="query" placeholder="enter a movie" value={query} onChange={(e) => setQuery(e.target.value)} />
             <button className="button" type="submit">Submit</button>
         </form>
     );
